c4d18ef123f7f523e042fbd3d031d8b997697d06
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-02 21:23:42 +0100

    Ejercicio 3 terminado

--- src/ejercicios5_Constructores/Main3.java
+++ src/ejercicios5_Constructores/Main3.java
@@ -1,10 +1,35 @@
 package ejercicios5_Constructores;
+import java.util.Scanner;
 
 public class Main3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner kb= new Scanner(System.in);
+		String name;
+		int matricula;
+		float note1;
+		float note2;
+		
+		System.out.println("Introduce tu nombre");
+		name= kb.next();
+		
+		System.out.println("Introduce tu numero de matricula");
+		matricula= kb.nextInt();
+		
+		Student3 s1= new Student3(matricula, name);
+		
+		System.out.println("Introduce la primera nota");
+		note1= kb.nextFloat();
+		
+		System.out.println("Introduce la segunda nota");
+		note2= kb.nextFloat();
 
+		s1.studentNotes(note1, note2);
+		s1.calcularMedia();
+		
+		System.out.println(s1.toString());
+		
 	}
 
 }
--- src/ejercicios5_Constructores/Student3.java
+++ src/ejercicios5_Constructores/Student3.java
@@ -1,5 +1,39 @@
 package ejercicios5_Constructores;
 
 public class Student3 {
+	private int matricula;
+	private String name;
+	private float note1;
+	private float note2;
+	private float media;
+	
+	public Student3(int matricula, String name) {
+		this.matricula= matricula;
+		this.name= name;
+		
+	}
+	
+	public Student3() {
+		
+	}
+	
+	public void studentNotes(float note1, float note2) {
+		this.note1= note1;
+		this.note2= note2;
+		
+	}
+	
+	public float calcularMedia() {
+		this.media= (this.note1+this.note2)/2;
+		
+		return media;
+	}
 
+	@Override
+	public String toString() {
+		return "Student3 [matricula=" + matricula + ", name=" + name + ", note1=" + note1 + ", note2=" + note2
+				+ ", media=" + media + "]";
+	}
+	
+	
 }
