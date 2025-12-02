76c0d8f618f1e8da37ebbf818730eb13761693f1
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-02 18:54:25 +0100

    Ejercicio 1 empezado

--- src/ejercicios5_toString/Main.java
+++ src/ejercicios5_toString/Main.java
@@ -3,7 +3,8 @@ package ejercicios5_toString;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Person persona1= new Person();
+		
 
 	}
 
--- src/ejercicios5_toString/Person.java
+++ src/ejercicios5_toString/Person.java
@@ -1,5 +1,48 @@
 package ejercicios5_toString;
 
 public class Person {
+	private String name;
+	private int age;
+    private float height;
+    private String work;
+/*
+    public Person1() {
+    	name= "sara";
+    	age= 20;
+    	height= 160; 
+    	work= "student";
+    }
+*/ 
+	public String getName(){
+		return name;
+	}
 
+	public void setName(String name){
+	    this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+	
 }
