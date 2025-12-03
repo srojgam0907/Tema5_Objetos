4a40dc23e147cae77ede2cd03a737c26dc884a86
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-03 19:07:21 +0100

    Ejercicio 1 corregido

--- src/ejercicios5_3_1Constructores/Main.java
+++ src/ejercicios5_3_1Constructores/Main.java
@@ -3,6 +3,11 @@ package ejercicios5_3_1Constructores;
 public class Main {
 
 	public static void main(String[] args) {
+		new Main().show();
+		
+	}
+	
+	public void show() {
 		Person persona1= new Person();
 
 		System.out.println(persona1.toString());
