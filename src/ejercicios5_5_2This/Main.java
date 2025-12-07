5fc13da55d77233c78bb91d716903379f8f74eca
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-07 18:34:44 +0100

    Ejercicio 2 this terminado

--- src/ejercicios5_5_2This/Main.java
+++ src/ejercicios5_5_2This/Main.java
@@ -3,8 +3,25 @@ package ejercicios5_5_2This;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Main().show();
 
+	}
+	
+	public void show() {
+		Person persona= new Person();
+		Person persona2= new Person("Selene", 20, 1.70f, "Programadora", 1500);
+		
+		System.out.printf("Persona 2: %.0f\n", persona2.getSalary());
+		
+		persona.setSalary(300);
+		persona.addSalary(persona2);
+		
+		System.out.printf("Persona 1: %.0f\n", persona.getSalary());
+		System.out.printf("Persona 2: %.0f\n", persona2.getSalary());
+		
+		persona.doubleSalary();
+		System.out.printf("Persona 1: %.0f", persona.getSalary());
+		
 	}
 
 }
--- src/ejercicios5_5_2This/Person.java
+++ src/ejercicios5_5_2This/Person.java
@@ -1,5 +1,77 @@
 package ejercicios5_5_2This;
 
 public class Person {
+	private String name;
+	private int age;
+    private float height;
+    private String work;
+    private float salary;
 
+    public Person() {
+    	this("unnamed",0,0.0f,"unemployment", 0f);
+    }
+    
+    public Person(String name, int age, float height, String work, float salary) {
+    	this.name= name;
+    	this.age= age;
+    	this.height= height;
+    	this.work= work;
+    	this.salary= salary;
+    }
+ 
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+	    this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+	
+	public float getSalary() {
+    	return salary;
+    }
+	
+	public void setSalary(float salary) {
+		this.salary = salary; 
+	}
+	
+	public float addSalary (Person persona) {
+    	salary+=persona.salary;
+    	
+    	return salary;
+    }
+	
+    public void doubleSalary () {
+    	salary = addSalary(this);
+    }
+	
+	@Override
+	public String toString() {
+		return String.format("Nombre: %s\nEdad: %d\nAltura: %.2f\nTrabajo: %s\nSalario: %2.f", name, age, height, work, salary); 
+	}
+	
 }
