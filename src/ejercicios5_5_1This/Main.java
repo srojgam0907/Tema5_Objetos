70b577273738233d3b2d60bdfb727d0a69c7174a
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-07 18:19:54 +0100

    Ejercicio 1 this terminado

--- src/ejercicios5_5_1This/Main.java
+++ src/ejercicios5_5_1This/Main.java
@@ -3,8 +3,16 @@ package ejercicios5_5_1This;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Main().show();
 
+	}
+	
+	public void show() {
+		Person persona1 = new Person();
+		Person persona2 = new Person("Pepé", 50, 1.8f, "Fontanero");
+		System.out.println(persona1.toString());
+		System.out.println(persona2.toString());
+		
 	}
 
 }
--- src/ejercicios5_5_1This/Person.java
+++ src/ejercicios5_5_1This/Person.java
@@ -1,5 +1,57 @@
 package ejercicios5_5_1This;
 
 public class Person {
+	private String name;
+	private int age;
+    private float height;
+    private String work;
 
+    public Person() {
+    	this("unnamed",0,0.0f,"unemployment");
+    }
+    
+    public Person(String name, int age, float height, String work) {
+    	this.name= name;
+    	this.age= age;
+    	this.height= height;
+    	this.work= work;
+    }
+ 
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+	    this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public String getWork() {
+		return work;
+	}
+
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Nombre: %s\nEdad: %d\nAltura: %.2f\nTrabajo: %s", name, age, height, work); 
+	}
+	
 }
