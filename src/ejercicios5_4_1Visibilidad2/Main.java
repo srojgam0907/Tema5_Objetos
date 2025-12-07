b4aeda97a94adc6bf7a9d4a50ed47749ebe44bcf
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-07 18:05:46 +0100

    clase main terminada

--- src/ejercicios5_4_1Visibilidad2/Main.java
+++ src/ejercicios5_4_1Visibilidad2/Main.java
@@ -1,10 +1,26 @@
 package ejercicios5_4_1Visibilidad2;
 
+import ejercicios5_4_1Visibilidad.Vehicles;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public void show() {
 
-	}
+        Vehicles car;
+        car = new Vehicles(4, "azul");
+         
+        //System.out.printf("El coche tiene %d ruedas", car.getWheelCount()); //no es visible porque es friendly
+        
+        car.accelerate(100); //es visible porque es public
+        
+        //System.out.printf("\nEl coche va a %.2f km/h", car.speed); //speed no es visible porque es private
+
+    }
+
+    public static void main(String[] args) {
+
+        new Main().show(); 
+
+    }
 
 }
