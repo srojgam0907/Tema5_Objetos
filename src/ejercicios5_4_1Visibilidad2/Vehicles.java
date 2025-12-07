dbcbdd94f23dfd257d15ae18aea4e9b91fad8923
srojgam0907 <srojgam0907@g.educaand.es>
2025-12-07 18:06:12 +0100

    clase vehicles terminada

--- src/ejercicios5_4_1Visibilidad2/Vehicles.java
+++ src/ejercicios5_4_1Visibilidad2/Vehicles.java
@@ -1,5 +1,43 @@
 package ejercicios5_4_1Visibilidad2;
 
 public class Vehicles {
+	private int wheelCount;
+    private double speed;
+    private String colour;
 
+    public Vehicles(int wheelCount, String colour) {
+        this.wheelCount = wheelCount;
+        this.colour = colour;
+        speed = 0;
+    }
+
+    int getWheelCount() {//Modificador de acceso friendly
+        return wheelCount;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void accelerate(double amount) {
+        speed += amount;
+    }
+
+    public void brake(double amount) {
+        speed -= amount;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Número de ruedas: %d, color: %s, velocidad: %.2f km/h", wheelCount, colour, speed);
+    }
+    
 }
